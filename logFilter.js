a76958df507662cce254e45be6e1e53b4530e184
John Carmichael <jc@stackeducation.com>
2019-06-24 15:11:08 -0400

    Initial Commit of files provided to students at start of exercise

---
+++ logFilter.js
@@ -0,0 +1,36 @@
+function filterLogsByEnv(logs, env) {
+  const filteredLogs = []
+
+  logs.forEach((log) => {
+    if (log.environment === env) {
+      filteredLogs.push(log)
+    }
+  })
+
+  return filteredLogs
+}
+
+function filterLogsOnDate(logs, dateFilter) {
+  const filteredLogs = []
+  const parsedDateFilter = new Date(dateFilter)
+
+  logs.forEach((log) => {
+    const timestampDate = new Date(log.timestamp)
+
+    if (parsedDateFilter.toDateString() === timestampDate.toDateString()) {
+      filteredLogs.push(log)
+    }
+  })
+
+  return filteredLogs
+}
+
+module.exports.filterLogs = function (logs, env, dateFilter) {
+  const envLogs = filterLogsByEnv(logs, env)
+
+  if (dateFilter) {
+    return filterLogsOnDate(envLogs, dateFilter)
+  } else {
+    return envLogs
+  }
+}
